5c3a829a7019ea16a81e814891ff575d4fed49e0
andree1985 <30987911+andree1985@users.noreply.github.com>
2019-01-29 12:24:09 +0200

    Fixed webpack configs

--- CCIS_Client/webpack.config.js
+++ CCIS_Client/webpack.config.js
@@ -11,33 +11,19 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   context: path.join(cwd, 'app'),
   mode,
-  optimization: {
-		// We no not want to minimize our code.
-    minimize: false,
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]App[\\/]JS[\\/]Config[\\/]/,
-          name: "config",
-          chunks: "all"
-        }
-      }
-    }
-  },
   entry: {
     app: ["babel-polyfill", './js/index.jsx'],
     silentRenew: ["./silent_renew/silent_renew.js"],
-    // I used to declare React as a separate bundle, but I believe SplitChunks takes care of that for me.
-    // react: [
-    //   'react',
-    //   'react-dom',
-    //   'react-router-dom',
-    //   'react-router',
-    //   'redux',
-    //   'react-redux',
-    //   'react-router-redux',
-    //   'history'
-    // ],
+    react: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'react-router',
+      'redux',
+      'react-redux',
+      'react-router-redux',
+      'history'
+    ],
 
     //Here I create my 'config' bundle, but these files still get bundled in the main bundle too, 
     //making this bundle useless
@@ -121,17 +107,17 @@ module.exports = {
       chunks: ["silentRenew"],
       filename: "silent_renew.html"
     }),
+    // new HtmlWebpackPlugin({
+    //   template: './index.ejs',
+    //   chunks: ["config"],
+    //   filename: "index.html"
+    // }),
     new webpack.DefinePlugin({
       CONSTANTS: {
         PROD: true,
         TEST: false,
         DEV: false
       }
-    }),
-    new HtmlWebpackPlugin({
-      template: './index.ejs',
-      chunks: ["config"],
-      filename: "index.html"
     }),
     new webpack.IgnorePlugin(/^(fs|ipc|ignore)$/)
   ]
